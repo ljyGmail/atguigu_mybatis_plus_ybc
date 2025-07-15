df2d0da77a4bf913de7496a5f629ccac66334be0
ljyGmail <liangjy881@gmail.com>
2025-07-15 18:18:30 +0900

    14 测试BaseMapper的删除(delete)功能

--- src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
+++ src/test/java/com/atguigu/mybatisplus/MyBatisPlusTest.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * ClassName: MyBatisPlusTest
@@ -37,5 +40,33 @@ public class MyBatisPlusTest {
         int result = userMapper.insert(user);
         System.out.println("result: " + result);
         System.out.println("id: " + user.getId());
+    }
+
+    @Test
+    public void testDeleteById() {
+        // 通过id删除用户信息
+        // DELETE FROM user WHERE id=?
+        int result = userMapper.deleteById(1945047573270102018L);
+        System.out.println("result: " + result);
+    }
+
+    @Test
+    public void testDeleteByMap() {
+        // 根据map中key指定的条件来删除用户信息
+        // DELETE FROM user WHERE name = ? AND age = ?
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "zs");
+        map.put("age", 23);
+        int result = userMapper.deleteByMap(map);
+        System.out.println("result: " + result);
+    }
+
+    @Test
+    public void testDeleteBatchIds() {
+        // 批量删除多个用户信息
+        // DELETE FROM user WHERE id IN ( ? , ? , ? )
+        List<Long> list = Arrays.asList(1L, 2L, 3L);
+        int result = userMapper.deleteBatchIds(list);
+        System.out.println("result: " + result);
     }
 }
