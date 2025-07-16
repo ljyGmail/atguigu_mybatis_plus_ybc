4d3171e23a110845fbbf51b07d8749a5082cc2b2
liangjinyong <liangjy881@gmail.com>
2025-07-16 14:51:56 +0900

    18 通用Service接口

---
+++ src/main/java/com/atguigu/mybatisplus/service/UserService.java
@@ -0,0 +1,16 @@
+package com.atguigu.mybatisplus.service;
+
+import com.atguigu.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * ClassName: UserService
+ * Package: com.atguigu.mybatisplus.service
+ * Description:
+ *
+ * @Author: ljy
+ * @Create: 2025. 7. 16. 오후 2:49
+ * @Version 1.0
+ */
+public interface UserService extends IService<User> {
+}
---
+++ src/main/java/com/atguigu/mybatisplus/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.atguigu.mybatisplus.service.impl;
+
+import com.atguigu.mybatisplus.mapper.UserMapper;
+import com.atguigu.mybatisplus.pojo.User;
+import com.atguigu.mybatisplus.service.UserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * ClassName: UserServiceImpl
+ * Package: com.atguigu.mybatisplus.service.impl
+ * Description:
+ *
+ * @Author: ljy
+ * @Create: 2025. 7. 16. 오후 2:49
+ * @Version 1.0
+ */
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+}
